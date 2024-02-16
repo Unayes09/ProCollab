801e98c52833a87d9300a6f62f32fd9e5043c171
nayem-2020331048 <n45islam@gmail.com>
2024-02-16 15:35:33 +0600

    register page fetching and verifying complete

--- Frontend/src/components/RegisterForm.jsx
+++ Frontend/src/components/RegisterForm.jsx
@@ -1,101 +1,84 @@
 import React, { useState, useEffect } from 'react';
 import registercss from './RegisterForm.module.css'
-
+import {  AiOutlineCloseCircle } from 'react-icons/ai'; // Import add and remove icons from react-icons
+import { useNavigate } from 'react-router-dom';
 // import { FaRegUserCircle } from "react-icons/fa";
 // import { FaLock } from "react-icons/fa";
 
 const RegisterForm = () => {
 
-        const [nameData, setname] = useState()
-        const [UsernameData, setUsername] = useState()
-        const [EmailData, setEmail] = useState()
-        const [PasswordData,setPassword]=useState()
-        const [ConfirmPasswordData, setConfirmPassword] = useState()
+    const navigate = useNavigate()
+  function routetoproject(){
+    navigate('/homepage')
+  }
+  function routetohome(){
+    navigate('/login')
+}
+
+    const [RegisterData, setRegisterData] = useState({
+   
+    name: '',
+    username: '',
+    email: [],
+    password: [],
+    interests: [],
     
-
+        });
     
-        // State to keep track of checked items
-        const [checkedItems, setCheckedItems] = useState({});
-
-        // Dummy data for checkboxes
-        const checkboxData = [
-            { id: 1, label: 'Option 1' },
-            { id: 2, label: 'Option 2' },
-            { id: 3, label: 'Option 3' },
-        ];
-
-        // Function to handle checkbox change
-        const handleCheckboxChange = (id) => {
-            setCheckedItems((prevCheckedItems) => ({
-                ...prevCheckedItems,
-                [id]: !prevCheckedItems[id],
-            }));
-        };
-        useEffect(() => {
-            console.log('Form data changed:', nameData);
-        }, [nameData]);
-        useEffect(() => {
-            console.log('Form data changed:', UsernameData);
-        }, [UsernameData]);
     
-        useEffect(() => {
-            console.log('Form data changed:', EmailData);
-        }, [EmailData]);
+    const tagOptions = [
+    'Programming Languages',
+    'Web Development',
+    'Data Structures and Algorithms',
+    'Artificial Intelligence and Machine Learning',
+    'Cybersecurity',
+    'Cloud Computing',
+    'Internet of Things (IoT)',
+    'Mobile App Development',
+    'Software Engineering',
+    'Data Science',
+    'DevOps',
+    'Robotics',
+    'Game Development',
+    'Quantum Computing',
+    'Emerging Technologies',
+  ];
     
-        useEffect(() => {
-            console.log('Form data changed:', PasswordData);
-        }, [PasswordData]);
-        useEffect(() => {
-            console.log('Form data changed:', ConfirmPasswordData);
-        }, [ConfirmPasswordData]);
-    
-        const handleSubmit = async (e) => {
-            e.preventDefault();
+ const handleTagChange = (selectedTag) => {
+    if (!RegisterData.interests.includes(selectedTag)) {
+      setRegisterData({ ...RegisterData, interests: [...RegisterData.interests, selectedTag] });
+    }
+  };
 
-            if (PasswordData != ConfirmPasswordData) {
-                alert("Password not matching")
-            }
-
-            // Get checked items as an array
-    const selectedItems = Object.keys(checkedItems).filter(
-      (key) => checkedItems[key]
-    );
-
+  const handleRemoveTag = (tagToRemove) => {
+    setRegisterData({ ...RegisterData, interests: RegisterData.interests.filter(tag => tag !== tagToRemove) });
+  };
+      
+       
     // Convert the array to a string to send to the backend
-    const selectedItemsString = selectedItems.join(',');
+  const handleSubmit = async(e) => {
+    e.preventDefault();
 
-    // Send the selected items to the backend (replace with your actual backend API call)
-    // Example: fetch('/api/submitCheckedItems', { method: 'POST', body: selectedItemsString });
+    try {
+        
+        await fetch('http://localhost:8000/auth/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(RegisterData)
 
-    console.log('Selected Items:', selectedItemsString);
-
-            try {
-                //console.log('Form data submitted:', formData);
-                const formData = {
-                    name:nameData,
-                    username: UsernameData,
-                    email: EmailData,
-                    password: PasswordData
-                }
-                let jsonData = "";
-                const response = await fetch('http://localhost:8000/auth/register', {
-                    method:'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(formData)
-                    
-                });
-                jsonData = await response.json()
-                if (response.ok) {
-                    console.log(jsonData)
-                } else {
-                    console.log(jsonData)
-                }
-            } catch (error) {
-                console.error('Error submitting form:', error);
-            }
-        };
+        })
+        .then(async response => {
+            const data = await response.json()
+            console.log(data)
+           
+        })
+      } catch (error) {
+          console.error('Error submitting form:', error);
+    }
+    console.log(JSON.stringify(RegisterData));
+  };
 
 
     return (
@@ -105,43 +88,49 @@ const RegisterForm = () => {
                 <form onSubmit={handleSubmit}>
                     <h1>Registeration Form</h1>
                         <div className={registercss.inputbox}>
-                        <input type="text" placeholder='Fullname' required name="Fullname" onChange={(e)=>setname(e.target.value)}/>
+                        <input type="text" placeholder='Name' required value={RegisterData.name} onChange={(e)=> setRegisterData({ ...RegisterData, name: e.target.value })}/>
                         
                     </div>
                         <div className={registercss.inputbox}>
-                        <input type="text" placeholder='Username' required name="Username" onChange={(e)=>setUsername(e.target.value)}/>
+                        <input type="text" placeholder='Username' required value={RegisterData.username} onChange={(e)=>setRegisterData({ ...RegisterData, username: e.target.value })}/>
                         
                     </div>
                         <div className={registercss.inputbox}>
-                        <input type="text" placeholder='Email' required name="Email" onChange={(e)=>setEmail(e.target.value)}/>
+                        <input type="email" placeholder='Email' required value={RegisterData.email} onChange={(e)=>setRegisterData({ ...RegisterData, email: e.target.value })}/>
                         
                     </div>
 
                         <div className={registercss.inputbox}>
-                        <input type="password" placeholder='Password' required onChange={(e) => setPassword(e.target.value)} /> 
+                        <input type="password" placeholder='Password' required value={RegisterData.password} onChange={(e) => setRegisterData({ ...RegisterData, password: e.target.value })} /> 
                     </div>
 
-                        <div className={registercss.inputbox}>
-                        <input type="password" placeholder='Confirm Password' required onChange={(e) => setConfirmPassword(e.target.value)} /> 
-                    </div>
+                        
 
 
-                     {checkboxData.map((checkbox) => (
-                        <div key={checkbox.id}>
-                             <label>
-                             <input
-                                  type="checkbox"
-                                 checked={checkedItems[checkbox.id] || false}
-                                onChange={() => handleCheckboxChange(checkbox.id)}
-                                 />
-                            {checkbox.label}
-                             </label>
-                        </div>
-                        ))}
-
+                     <div className={registercss.inputgroup}>
+                            <label>Tags</label>
+                            <select onChange={(e) => handleTagChange(e.target.value)}>
+                             <option value="">Select Tag</option>
+                    {tagOptions.map((tag, index) => (
+                    <option key={index} value={tag}>
+                            {tag}
+                        </option>
+            ))}
+                            </select>
+                            
+            <div className={registercss.tags}>
+            {RegisterData.interests.map((tag, index) => (
+              <div key={index} className={registercss.tag}>
+                {tag}
+                <AiOutlineCloseCircle className={registercss.removeicon} onClick={() => handleRemoveTag(tag)} />
+              </div>
+            ))}
+          </div>
+         
+        </div>
 
                     
-                    <button type="submit">Register</button>
+                    <button type="submit" onClick={handleSubmit}>Register</button>
 
                         <div className={registercss.registerlink}>
                         <p>Already Have an account? <a href='/signin'>Login</a></p>
